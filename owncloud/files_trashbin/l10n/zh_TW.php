4c6c4de554b7f9c70ca7c26b1a2c10fc4d8acce5
tsung <s6414073@yahoo.com.tw>
2013-12-22 01:58:21 +0800

    modified LANG

--- owncloud/files_trashbin/l10n/zh_TW.php
+++ owncloud/files_trashbin/l10n/zh_TW.php
@@ -1,10 +1,13 @@
 <?php $TRANSLATIONS = array(
 "Delete permanently" => "永久刪除",
 "Name" => "名稱",
+"Deleted" => "刪除時間",
 "1 folder" => "1 個資料夾",
 "{count} folders" => "{count} 個資料夾",
 "1 file" => "1 個檔案",
 "{count} files" => "{count} 個檔案",
+"Nothing in here. Your trash bin is empty!" => "您的回收桶是空的！",
+"Restore" => "還原",
 "Delete" => "刪除",
 "Deleted Files" => "已刪除的檔案"
 );
